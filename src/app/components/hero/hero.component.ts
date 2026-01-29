e252ba8137cc82ad9a02f0a95453d37a34a335c6
LaurentMbuyu <laurent.mbuyu@takeda.com>
2026-01-29 16:52:32 +0200

    [Update]: Hero section has been updated.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -15,9 +15,9 @@ export class HeroComponent {
   slides = [
     {
       category: 'HOME OF LAW & ORDER',
-      title: 'DRC: JUDICIAL YEAR BEGINS AT THE COURT OF CASSATION FOR THE 2025-2026 TERM',
+      title: 'DRC: JUDICIAL YEAR BEGINS AT THE STATE COUNCIL FOR THE 2025-2026 TERM',
       description: 'This video covers the formal opening of the judicial year for 2025-2026 . In the presence of President Félix Tshisekedi, magistrates resumed their duties after the judicial recess.',
-      link: 'OPENING OF THE JUDICIAL YEAR AT THE COURT OF CASSATION - YouTube',
+      link: 'OPENING OF THE JUDICIAL YEAR AT THE STATE COUNCIL - YouTube',
       buttonText: 'Watch Live Proceeding on Youtube',
       buttonUrl: '#'
     }
