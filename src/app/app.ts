062ecf1abcd3c9c40e27c7d44fdac3457739ea7e
LaurentMbuyu <laurent.mbuyu@takeda.com>
2026-02-19 22:28:07 +0200

    [New]: New animation.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { I18nPipe } from './i18n/i18n.pipe';
 
@@ -11,4 +11,10 @@ import { I18nPipe } from './i18n/i18n.pipe';
 })
 export class App {
   protected readonly title = signal('state-council-portal');
+
+  constructor(private readonly router: Router) {}
+
+  isFilingActive(): boolean {
+    return this.router.url.startsWith('/steps') || this.router.url.startsWith('/filing');
+  }
 }
