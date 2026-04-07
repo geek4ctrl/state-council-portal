899dd4215da6cffa63122d8fa894e233f904daaa
Laurent Mbuyu <laurent.mb19@gmail.com>
2026-04-07 17:23:53 +0200

    [New]: Mission texts have been modified.

--- src/app/pages/presentation-section/presentation-section.component.ts
+++ src/app/pages/presentation-section/presentation-section.component.ts
@@ -504,13 +504,6 @@ type GreffeFirstPresident = {
               <div class="section-card">
                 <p>{{ 'about.legal.detail.paragraph1' | i18n }}</p>
                 <p>{{ 'about.legal.detail.paragraph2' | i18n }}</p>
-                <ul>
-                  <li>{{ 'about.legal.detail.list1.1' | i18n }}</li>
-                  <li>{{ 'about.legal.detail.list1.2' | i18n }}</li>
-                  <li>{{ 'about.legal.detail.list1.3' | i18n }}</li>
-                  <li>{{ 'about.legal.detail.list1.4' | i18n }}</li>
-                  <li>{{ 'about.legal.detail.list1.5' | i18n }}</li>
-                </ul>
               </div>
             } @else if (isFondements()) {
               <div class="section-card">
